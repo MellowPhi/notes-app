8b38559cc2ad0a82565310487823b966b6530fa0
MellowPhi <ombt14@gmail.com>
2023-09-27 17:19:46 +1000

    first commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBb3e029ynC8JtqzqsvTsaGC8A25NtQu88",
+  authDomain: "react-notes-app-a9a7b.firebaseapp.com",
+  projectId: "react-notes-app-a9a7b",
+  storageBucket: "react-notes-app-a9a7b.appspot.com",
+  messagingSenderId: "158567397655",
+  appId: "1:158567397655:web:86df741957623b4a701f52",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes");
